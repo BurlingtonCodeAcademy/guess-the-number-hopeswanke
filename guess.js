ff2b422cc306388024b04ec085020ba8c9ad77df
hopeswanke <hopeswanke@gmail.com>
2020-11-06 14:49:03 -0500

    updating 11/5

--- guess.js
+++ guess.js
@@ -1,96 +1,146 @@
-const readline = require('readline');
+// ---------------- background functionality ---------------
+// import readline for ask functionality
+const readline = require("readline");
 const rl = readline.createInterface(process.stdin, process.stdout);
-
+// function for await ask functionality
 function ask(questionText) {
-    return new Promise((resolve, reject) => {
-        rl.question(questionText, resolve);
-    });
+  return new Promise((resolve, reject) => {
+    rl.question(questionText, resolve);
+  });
 }
-
-choice()
-async function choice() {
-    let start = await ask('human guess or computer guess? ')
-    if (start === 'human guess') {
-       return startComp()
-    }
-    else if (start === 'computer guess') {
-        return startHuman()
-    }
-}
-
+// function to generate random integer between min and max set by user
 function randomInt(min, max) {
-    return Math.floor((Math.random()/2) * (max - min + 1) + min)
+  return Math.floor((Math.random() / 2) * (max - min + 1) + min);
 }
+// --------------------- Comp/Human choice -----------------
+// calls choice function
+choice();
+// lets user choose which version to play
+async function choice() {
+  // set question to variable
+  let start = await ask("human guess or computer guess? ");
+  // if they choose human guess return startComp function
+  
+  if (start === "human guess") {
+    return startHumanGuess();
+  } else if (start === "computer guess") {
+    // if they choose computer guess return startHuman function
+    return startComputerGuess();
+  } else{
+    console.log('Please enter a valid option')
+    process.exit()
+  }
 
-startComp();
-async function startComp() {
-    let game = await ask("Are you ready to start the game?")
-    let question = await ask('What number do you want your range till? ')
-    console.log('Alright I am thinking of a number ')
-    let min = 1
-    let max = question 
-    let compNumber = randomInt(min, max)
-    let playerGuess = await ask('What\'s your Guess? ')
-    playerGuess = parseInt(playerGuess)
-    if (playerGuess === compNumber) {
-        console.log('Damn you won!! Until next time foe!!')
-        process.exit()
+
+// -------------------- Computer Guessing Function ---------
+// calls human guessing function
+startHumanGuess();
+async function startHumanGuess() {
+  // sets max range to a variable
+  let question = await ask("What number do you want your range till? ");
+  // creates game fluidity
+  console.log("Alright I am thinking of a number ");
+  // hard codes min to 1
+  let min = 1;
+  // sets max to number inputted above
+  let max = question;
+  // sets computer number to random number using function defined above
+  let compNumber = randomInt(min, max);
+  // sets player guess to variable
+  let playerGuess = await ask("What's your Guess? ");
+  // parses guess from a string to a number
+  playerGuess = parseInt(playerGuess);
+  // if player guess is  on first try correct
+  if (playerGuess === compNumber) {
+    console.log("Damn you won!! Until next time foe!!");
+    process.exit();
+  }
+  // if player guess is incorrect
+  else {
+    // creates loop for player to keep guessing until correct
+    while (playerGuess !== compNumber) {
+      // if the number is too low
+      if (playerGuess < compNumber) {
+        console.log("too low, try again");
+      }
+      // if the guess it too high
+      else if (playerGuess > compNumber) {
+        console.log("sorry! too high");
+      }
+      // if player is correct
+      else {
+        console.log("Yay you win!");
+        process.exit();
+      }
+      // lets player guess again
+      playerGuess = await ask("Care to try again? ");
+      // if player answers no instead of a number exits game
+      if (playerGuess === "no") {
+        console.log("WASTED");
+        process.exit();
+      }
     }
-    else {
-        while (playerGuess !== compNumber) {
-            if (playerGuess < compNumber) {
-                console.log('too low, try again')
-            }
-
-            else if (playerGuess > compNumber) {
-                console.log('sorry! too high')
-            }
-            else {
-                console.log('Yay you win!')
-                process.exit()
-            }
-            playerGuess = await ask('Care to try again? ')
-            if (playerGuess === 'no') {
-                console.log('WASTED')
-                process.exit()
-            }
-
-        }
-    }
+  }
 }
-
-startHuman();
-async function startHuman() {
-    let question = await ask('What number do you want your range till? ')
-    let secretNumber = await ask("What is your secret number?");
-    let min = 1
-    let max = question 
-    let compGuess = randomInt(min, max)
-    let response = await ask('Is this your number? ' + compGuess + ' yes or no? ')
-    if (response === 'yes') {
-        console.log('YAY!')
-        process.exit()
+// --------------------- Human Guessing Function -----------
+// calls computer guessing function
+startComputerGuess();
+async function startComputerGuess() {
+  // sets range set by player to variable
+  let question = await ask("What number do you want your range till? ");
+  // sets players number to variable
+  let secretNumber = await ask("What is your secret number?");
+  // sets min
+  let min = 1;
+  // sets max to variable player set above
+  let max = question;
+  // sets computer guess to variable
+  let compGuess = randomInt(min, max);
+  // puts above variable in question from which allows player to say yes or no then sets response to variable
+  let response = await ask(
+    "Is this your number? " + compGuess + " yes or no? "
+  );
+  // if computer is correct first try
+  if (response === "yes") {
+    console.log("YAY!");
+    process.exit();
+  }
+  // if computer is incorrect
+  else {
+    // creates loop for computer to keep guessing
+    while (response !== "yes") {
+      // sets clarifying question to variable
+      let answer = await ask("Is your number higher or lower? ");
+      // if number is higher
+      if (answer === "higher") {
+        //   resets the min to computer guess plus one
+        min = compGuess + 1;
+        // computes new guess with new min
+        compGuess = randomInt(min, max);
+      }
+      // if number is lower
+      else if (answer === "lower") {
+        // sets max to computer guess minus one
+        max = compGuess - 1;
+        // regenerates guess with new max
+        compGuess = randomInt(min, max);
+      }
+      // re-asks question with new number
+      response = await ask(
+        "Is this your number? " + compGuess + " yes or no? "
+      );
+      // if correct leaves loop and exits game
+      if (response === "yes") {
+        console.log("YAY!");
+        process.exit();
+      }
+      // cheaters catch if player incorrectly answers no to computers guess
+      else if (compGuess == secretNumber && response !== "yes") {
+        console.log(
+          "Better call the fire department...cause your pants are on fire!!!!"
+        );
+        process.exit();
+      }
     }
-    else {
-        while (response !== 'yes') {
-            let answer = await ask('Is your number higher or lower? ')
-            if (answer === 'higher') {
-                min = compGuess + 1
-                compGuess = randomInt(min, max)
-            }
-            else if (answer === 'lower') {
-                max = compGuess - 1
-                compGuess = randomInt(min, max)
-            }
-            response = await ask('Is this your number? ' + compGuess + ' yes or no? ')
-            if (response === 'yes') {
-                console.log('YAY!')
-                process.exit()
-            }
-            else if (compGuess == secretNumber && response !== "yes") {
-                console.log('Better call the fire department...cause your pants are on fire!!!!')
-                process.exit()
-            }
-        }
-    }
+  }
 }
